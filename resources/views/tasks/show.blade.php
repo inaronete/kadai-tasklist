426552e262c548a6ff412de779263b634e27dc52
inaronete <be.here.now1217@gmail.com>
2018-08-08 08:09:46 +0000

    kadai-tasklist

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -7,7 +7,7 @@
     
     <p>{{ $task->content }}</p>
     
-    {!! link_to_route('tasks.edit','このメッセージを編集',['id' => $message->id]} !!}
+    {!! link_to_route('tasks.edit','このメッセージを編集',['id' => $task->id]) !!}
     
     {!! Form::model($task,['route' => ['tasks.destroy',$task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}

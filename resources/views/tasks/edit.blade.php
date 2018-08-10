794887ae886e980d76f91ba14f12eea64f0f303a
inaronete <be.here.now1217@gmail.com>
2018-08-10 13:06:34 +0000

    add colum

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,8 +3,12 @@
 @section('content')
 
     <h1>id: {{ $task->id }}のタスク編集ページ</h1>
+
     
-    {!! Form::model($task,['route' => ['task.update',$task->id],'method' => 'put']) !!}
+    {!! Form::model($task,['route' => ['tasks.update',$task->id],'method' => 'put']) !!}
+    
+        {!! Form::label('status','ステータス:') !!}
+        {!! Form::text('status') !!}
     
         {!! Form::label('content','タスク:') !!}
         {!! Form::text('content') !!}

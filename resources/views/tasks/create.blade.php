42165d398e808c3b362fba730017e8cabddf8acf
inaronete <be.here.now1217@gmail.com>
2018-08-14 08:27:56 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,25 +2,27 @@
 
 @section('content')
 
-    <h1>タスク新規作成ページ</h1>
     
-    <form class="form-horizontal">
-          <div class="row">
-               <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8　col-offset-lg-3 col-lg-6">
-       
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        <div class="form-group">
-        {!! Form::label('status','タイトル:') !!}
-        {!! Form::text('status', null, ['class' => 'form-control']) !!}    
     
-        </div>
-        <div class="form-group">
-        {!! Form::label('content','タスク:') !!}
-        {!! Form::text('content', null, ['class' =>'form-control']) !!}
-        </div>
-        
-        {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
     
-    {!! Form::close() !!}
+    <div class="row">
+       <div class="col-xs-12 col-sm-offset-2 col-sm-8  col-md-offset-2 col-md-8　col-lg-offset-3 col-lg-6">
+                   
+       <h1>タスク新規作成ページ</h1>
+            
+            {!! Form::model($task, ['route' => 'tasks.store', 'class' => 'form-horizontal']) !!}
+                <div class="form-group">
+                {!! Form::label('status','タイトル:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}    
+                </div>
+                <div class="form-group">
+                {!! Form::label('content','タスク:') !!}
+                {!! Form::text('content', null, ['class' =>'form-control']) !!}
+                </div>
+            
+            {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
+    　　    {!! Form::close() !!}
+　     </div>
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,15 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8  col-md-offset-2 col-md-8　col-lg-offset-3 col-lg-6">
     <h1>id: {{ $task->id }}のタスク編集ページ</h1>
-
-    
-    <form class="form-horizontal">
-          <div class="row">
-               <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8　col-offset-lg-3 col-lg-6">
-    
-    {!! Form::model($task,['route' => ['tasks.update',$task->id],'method' => 'put']) !!}
+    {!! Form::model($task,['route' => ['tasks.update',$task->id],'method' => 'put', 'class' => 'form-horizontal']) !!}
       
           <div class="form-group">
                   {!! Form::label('status','タイトル:') !!}

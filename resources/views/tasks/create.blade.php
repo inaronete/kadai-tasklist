04193f782a18424ec04a71c4f3cf355fa41c5e41
inaronete <be.here.now1217@gmail.com>
2018-08-14 05:18:24 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,27 +4,20 @@
 
     <h1>タスク新規作成ページ</h1>
     
-    
+    <form class="form-horizontal">
+          <div class="row">
+               <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8　col-offset-lg-3 col-lg-6">
        
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-    <form class="form-horizontal">
-      <div class="form-group">
-          <div class="row">
-               <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8 col-offset-lg-3 col-lg-6">
+        <div class="form-group">
         {!! Form::label('status','タイトル:') !!}
         {!! Form::text('status', null, ['class' => 'form-control']) !!}    
-               </div>    
-          </div>
-      </div>
-      <div class="form-group">
-          <div class="row">
-                <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8 col-offset-lg-3 col-lg-6">
+    
+        </div>
+        <div class="form-group">
         {!! Form::label('content','タスク:') !!}
         {!! Form::text('content', null, ['class' =>'form-control']) !!}
-               </div>    
-          </div>
-      </div>
+        </div>
         
         {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
     
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,28 +5,26 @@
     <h1>id: {{ $task->id }}のタスク編集ページ</h1>
 
     
-    {!! Form::model($task,['route' => ['tasks.update',$task->id],'method' => 'put']) !!}
-      <form class="form-horizontal">
-          <div class="form-group">
+    <form class="form-horizontal">
           <div class="row">
-               <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8 col-lg-6">
-        {!! Form::label('status','タイトル:') !!}
+               <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8　col-offset-lg-3 col-lg-6">
+    
+    {!! Form::model($task,['route' => ['tasks.update',$task->id],'method' => 'put']) !!}
+      
+          <div class="form-group">
+                  {!! Form::label('status','タイトル:') !!}
         {!! Form::text('status', null, ['class' => 'form-control']) !!}
-            　 </div>    
-          </div>
       </div>
       <div class="form-group">
-          <div class="row">
-                <div class="col-xs-12 col-offset-sm-2 col-sm-8  col-offset-md-2 col-md-8 col-lg-6">
         {!! Form::label('content','タスク:') !!}
         {!! Form::text('content', null, ['class' => 'form-control']) !!}
-               </div>    
-          </div>
       </div>    
         
         {!! Form::submit('更新') !!}
         
     {!! Form::close() !!}
+        </div>
+    </div>
     
 
 @endsection
